57f51ea20d31d72a8ba9a456d39154a895e86d0b
chungddong <chungman010918@gmail.com>
2024-06-25 02:05:25 +0900

    배포 파일 위한 수정

--- src/main/java/com/sophra/chungmusic/WebContorller.java
+++ src/main/java/com/sophra/chungmusic/WebContorller.java
@@ -1,14 +1,33 @@
 package com.sophra.chungmusic;
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.core.io.Resource;
 
-@Controller
-public class WebContorller {
+@Configuration
+public class WebContorller implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-    @RequestMapping(value = {"/", "/error"})
-    public String forward() {
-        return "forward:/index.html";
+        registry.addResourceHandler("/tag/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                            Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
-    
+
 }
